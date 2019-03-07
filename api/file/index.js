85121dd42bfdccf11cb35345056365a8ddbccd9a
caomeng <caomeng@9188.com>
2019-03-07 16:04:15 +0800

    1

---
+++ api/file/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Mn on 
+ */
+var express = require('express')
+var controller = require('./file.controller')
+const auth = require('../../auth/auth.service')
+
+var router = express.Router()
+
+router.get('/', auth.isAuthenticated(), controller.index)
+router.get('/theme/:id', auth.isAuthenticated(), controller.getByThemeId)
+router.get('/:id', controller.show)
+router.post('/', controller.create)
+router.delete('/:id', auth.isAuthenticated(), controller.destroy)
+
+module.exports = router
---
+++ controller/pages.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Mn on 
+ */
+var Pages = require('../api/pages/pages.model')
+
+module.exports.findByLoginId = (req, res) => {
+    var loginId = req.user.loginId
+    return Pages.find({ loginId: loginId }).exec()
+}
+
+// Gets a single Pages from the DB
+module.exports.findById = (id) => {
+    return Pages.findById(id).exec()
+}
